60263e843fd8acefc30b759f4ecb0fc7d23e9d75
DontStopPrograming <01dyamond01@gmail.com>
2023-06-20 17:34:33 -0500

    protected admin

--- src/routers/MyRoutes.jsx
+++ src/routers/MyRoutes.jsx
@@ -1,42 +1,43 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { Home } from '../pages/Home'
-import { Login } from '../pages/Login'
-import { UserAuth } from '../context/AuthContext'
-import { Perfil } from '../pages/Perfil'
-import { Protector } from '../Components/Protector'
-import { Admin } from '../pages/Admin'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Login } from '../pages/Login';
+import { UserAuth } from '../context/AuthContext';
+import { Perfil } from '../pages/Perfil';
+import { Protector } from '../Components/Protector';
+import { Admin } from '../pages/Admin';
 
-export function MyRoutes() { 
-  const { user } = UserAuth()
-  const RequireAuth = ({children}) => {
-    return user?children : <Navigate to = '/login' />
-  }
+export function MyRoutes() {
+  const { user } = UserAuth();
+
+  const RequireAuth = ({ children }) => {
+    return user ? children : <Navigate to="/login" />;
+  };
+
+  const RequireAdmin = ({ children }) => {
+    return user?.email === 'lylylatez@gmail.com' ? children : <Navigate to="/" />;
+  };
+
   return (
     <BrowserRouter>
-     <Routes>
-        <Route path = '/' element = {<RequireAuth>
-            {user?.email === 'lylylatez@gmail.com' ? (
-              <Admin />
-            ) : (
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <RequireAdmin>
+                <Admin />
+              </RequireAdmin>
               <Home />
-              )}
-          </RequireAuth>}/>
-        
-        <Route path = '/login' element = {<Login />} />
-        <Route path = '/admin' element = {<Admin />} />
-        <Route path = '/home' element = {
-          
-          <Protector>
-            <Home />
-          </Protector>
-        
-        } />
-        <Route path = '/perfil' element = {<Protector>
-          
-          <Perfil />
-        </Protector>} />
-        
-     </Routes>
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<RequireAdmin><Admin /></RequireAdmin>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/perfil" element={<Protector><Perfil /></Protector>} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
+
