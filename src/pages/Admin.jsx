0d1786f0848b5fdafaeee1b07453501183acabbf
DontStopPrograming <01dyamond01@gmail.com>
2023-06-21 18:08:52 -0500

    last last

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,33 +1,33 @@
-// Admin.js
-import React, { useState, useEffect } from 'react';
-import { Navigate, Link } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
+import { useState, useEffect } from 'react'
+import { UserAuth } from '../context/AuthContext'
+import { Navigate } from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
 
 export function Admin() {
-  const { user, logOut } = UserAuth();
+  const { user, logOut } = UserAuth()
 
   const cerrarSesion = async () => {
     try {
       await logOut();
-      Navigate('/login');
+      Navigate('/login')
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const [taskCount, setTaskCount] = useState(0);
   const [images, setImages] = useState([]);
   const [descriptions, setDescriptions] = useState([]);
   const [editModes, setEditModes] = useState([]);
-  const [lastUpdated, setLastUpdated] = useState(0);
 
   const handleTaskCountChange = (e) => {
-    const count = parseInt(e.target.value);
-    setTaskCount(count);
-    setImages(Array(count).fill(null));
-    setDescriptions(Array(count).fill(''));
-    setEditModes(Array(count).fill(false));
-  };
+    const count = parseInt(e.target.value)
+    setTaskCount(count)
+    setImages(Array(count).fill(null))
+    setDescriptions(Array(count).fill(''))
+    setEditModes(Array(count).fill(false))
+  }
 
   const handleImageChange = (e, index) => {
     const file = e.target.files[0];
@@ -38,6 +38,8 @@ export function Admin() {
       const updatedImages = [...images];
       updatedImages[index] = imageDataUrl;
       setImages(updatedImages);
+
+      localStorage.setItem('adminImages', JSON.stringify(updatedImages));
     };
 
     reader.readAsDataURL(file);
@@ -53,7 +55,6 @@ export function Admin() {
     const updatedEditModes = [...editModes];
     updatedEditModes[index] = false;
     setEditModes(updatedEditModes);
-    setLastUpdated(Date.now());
   };
 
   const handleImageEdit = (index) => {
@@ -66,7 +67,6 @@ export function Admin() {
     const storedTaskCount = localStorage.getItem('adminTaskCount');
     const storedImages = localStorage.getItem('adminImages');
     const storedDescriptions = localStorage.getItem('adminDescriptions');
-    const storedLastUpdated = localStorage.getItem('lastUpdated');
 
     if (storedTaskCount) {
       setTaskCount(parseInt(storedTaskCount));
@@ -76,18 +76,14 @@ export function Admin() {
       setImages(JSON.parse(storedImages));
       setDescriptions(JSON.parse(storedDescriptions));
     }
-
-    if (storedLastUpdated) {
-      setLastUpdated(parseInt(storedLastUpdated));
-    }
   }, []);
 
-  useEffect(() => {
+  const handleSaveChanges = () => {
     localStorage.setItem('adminTaskCount', taskCount.toString());
     localStorage.setItem('adminImages', JSON.stringify(images));
     localStorage.setItem('adminDescriptions', JSON.stringify(descriptions));
-    localStorage.setItem('lastUpdated', lastUpdated.toString());
-  }, [taskCount, images, descriptions, lastUpdated]);
+    alert('Se han guardado los cambios');
+  };
 
   return (
     <>
@@ -105,32 +101,74 @@ export function Admin() {
           />
 
           {taskCount > 0 && (
-            <div style={{ display: 'grid', gridTemplateColumns: `repeat(${taskCount}, 1fr)`, gap: '10px', marginTop: '20px' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: `repeat(${taskCount}, 1fr)`, gap:'10px', marginTop: '20px' }}>
               {Array(taskCount)
                 .fill()
                 .map((_, index) => (
-                  <div key={index} style={{ display: 'flex',
-                  flexDirection: 'column',
-                  width: '250px',
-                  height: '250px',
-                  padding: '10px 20px',
-                  background: '#232323',
-                  color: '#fff',
-                  fontSize: '1.2rem', }}>
-                    <input type='file' onChange={(e) => handleImageChange(e, index)} />
-                    {images[index] && <img src={images[index]} alt='task' style={{ width: '100px' }} />}
-                    <textarea value={descriptions[index]} onChange={(e) => handleImageDescriptionChange(e, index)}></textarea>
-                    {!editModes[index] ? (
-                      <button onClick={() => handleImageEdit(index)}>Editar</button>
-                    ) : (
-                      <button onClick={() => handleImageSubmit(index)}>Guardar cambios</button>
+                  <div key={index} style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      width: '250px',
+                      height: '250px',
+                      padding: '10px 20px',
+                      background: '#232323',
+                      color: '#fff',
+                      fontSize: '1.2rem'
+                      }}
+                    >
+                    {/* Modificación: Se verifica si la imagen es un objeto o una cadena */}
+                    {images[index] && (
+                      <>
+                        <div style={{ height:'50%', overflow:'hidden'}}>
+                          {/* Modificación: Se utiliza URL.createObjectURL para mostrar la imagen */}
+                          <img src={typeof images[index] === 'object' ? URL.createObjectURL(images[index]) : images[index]} alt="imagen" style={{ width: '100%', height: '100%' }} />
+                        </div>
+
+                        <div style={{ height:'50%', overflow:'hidden'}}>
+                          <p style={{ wordWrap: 'break-word' }}>{descriptions[index]}</p>
+                        </div>
+
+                        {editModes[index] ? (
+                          <>
+                            <input type='file' accept='image/*' onChange={(e) => handleImageChange(e, index)} />
+                            <input
+                              type='text'
+                              placeholder='Descripción de la imagen'
+                              value={descriptions[index]}
+                              onChange={(e) => handleImageDescriptionChange(e, index)}
+                            />
+                            <button onClick={() => handleImageSubmit(index)}>Aceptar</button>
+                          </>
+                        ) : (
+                          <button onClick={() => handleImageEdit(index)}>Editar</button>
+                        )}
+                      </>
+                    )}
+                    {!images[index] && (
+                      <>
+                        <input type='file' accept='image/*' onChange={(e) => handleImageChange(e, index)} />
+                        <input
+                          type='text'
+                          placeholder='Descripción de la imagen'
+                          value={descriptions[index] || ''}
+                          onChange={(e) => handleImageDescriptionChange(e, index)}
+                        />
+                        {descriptions[index] && <p style={{ wordWrap: 'break-word' }}>{descriptions[index]}</p>}
+                        <button onClick={() => handleImageSubmit(index)}>Aceptar</button>
+                      </>
                     )}
                   </div>
                 ))}
             </div>
           )}
+          <button onClick={handleSaveChanges}>Guardar cambios</button>
+          <button onClick={cerrarSesion}>Cerrar Sesión</button>
         </div>
       )}
+      {!user && <Navigate to="/login" />}
+      <button>
+        <Link to = '/home'> Ir al Home </Link>
+      </button>
     </>
   );
 }
